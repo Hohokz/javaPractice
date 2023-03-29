2dfde4ba1c940895e89629861493990ae8d7acbe
hohokz <hohokz@hotmail.com>
2023-03-29 15:32:19 +0700

    fix : fix logic on leap year

--- src/LeapYear.java
+++ src/LeapYear.java
@@ -7,14 +7,12 @@ public class LeapYear {
         System.out.println("Enter Year");
         int year = getYear.nextInt();
 
-        if((year % 4) != 0){
-            System.out.println(year + " is not a leap year ");
-        } else if ((year %100) != 0) {
-            System.out.println(year + " is not a leap year ");
-        } else if ((year %400) != 0) {
-            System.out.println(year + " is not a leap year ");
-        }else {
+        if((year % 4) == 0 && (year % 100) != 0){
             System.out.println(year + " is a leap year ");
+        } else if ((year %100) == 0 && (year % 400) == 0) {
+            System.out.println(year + " is a leap year ");
+        }else {
+            System.out.println(year + " is not a leap year ");
         }
     }
 }
